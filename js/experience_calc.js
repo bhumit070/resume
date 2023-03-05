54831c6a2dec2d38cdd79d1f0bc731cd23125795
bhumit070 <ganatrabhoomit070@gmail.com>
2023-03-06 00:34:23 +0530

    added devicons

---
+++ js/experience_calc.js
@@ -0,0 +1,13 @@
+const experienceYearSpan = document.querySelector('#experience-years');
+const currentDate = new Date()
+const startingDate = new Date(2020, 11, 17); // 17 december 2020
+// find years of gap in current date in starting date
+let yearsOfExperience = (currentDate.getFullYear() - startingDate.getFullYear()) - 1;
+const currentMonth = currentDate.getMonth();
+console.log(currentMonth, 'currentMonth')
+if (currentMonth === 11) {
+	yearsOfExperience += 1;
+} else {
+	yearsOfExperience = `${yearsOfExperience}.${currentMonth + 1}`;
+}
+experienceYearSpan.innerText = yearsOfExperience;
