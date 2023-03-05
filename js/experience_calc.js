1ff43fd786f4a71dc945d858fc0f265992552858
bhumit070 <ganatrabhoomit070@gmail.com>
2023-03-06 00:49:43 +0530

    removed unused console.logs and WIP: data loader file

--- js/experience_calc.js
+++ js/experience_calc.js
@@ -4,7 +4,6 @@ const startingDate = new Date(2020, 11, 17); // 17 december 2020
 // find years of gap in current date in starting date
 let yearsOfExperience = (currentDate.getFullYear() - startingDate.getFullYear()) - 1;
 const currentMonth = currentDate.getMonth();
-console.log(currentMonth, 'currentMonth')
 if (currentMonth === 11) {
 	yearsOfExperience += 1;
 } else {
